1f586acb61d82e5f11f142e891de16a204102ba1
Mostafa Ahmed <mostafamohamedhamed@outlook.com>
2023-11-20 14:43:54 +0200

    before using sub component

--- HelmesCustomerManager.Domain/Entities/Sector.cs
+++ HelmesCustomerManager.Domain/Entities/Sector.cs
@@ -4,9 +4,9 @@ public class Sector
 {
     public required Guid Id { get; set; }
 
-    public required string Name { get; set; }
+    public string? Name { get; set; }
 
     public Guid? ParentSectorId { get; set; }
 
-    public IEnumerable<Sector> SubSectors { get; set; }
+    public IEnumerable<Sector>? SubSectors { get; set; }
 }
